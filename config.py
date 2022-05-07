662b500457af0596b4817809295683c3896efaa9
splaiser <ybrbx@yandex.ru>
2022-05-07 11:40:06 +0300

    fix

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 DATABASE = {
     'drivername': 'sqlite',
-    # 'host': 'localhost',
-    # 'port': '5432',
-    # 'username': 'TG_ADMIN',
-    # 'password': 'Passwd1234',
     'database': 'data_base/rooms_booking.db'
 }
 
